1bdc9706b407c8dbb95f57980b6e40fdedaa63c1
Aranarora <aranarora007@gmail.com>
2023-10-17 16:56:37 -0700

    Add image in div with class home-logo in contact-list

--- views/partials/foot.php
+++ views/partials/foot.php
@@ -1,6 +1,11 @@
 <footer class="content-wrap">
             <div class="contact-list">
-            
+            <div class="home-logo">
+                    <figure>
+                        <a href="/">
+                            <img src="imagefolder/runrentallogo.jpeg" width="120" height="120" style="width: 80%; height: auto;" alt="image placeholder" /></a>
+                    </figure>
+                </div>
             </div>
     </footer>
 </body>
