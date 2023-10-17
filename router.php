2f37b123b8e3596e58ab021b4f24298c9256959a
Aranarora <aranarora007@gmail.com>
2023-10-17 13:29:09 -0700

    Create a array of path to link the file with url path's

--- router.php
+++ router.php
@@ -1,3 +1,10 @@
 <?php
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+    '/ride' => 'controllers/ride.php',
+];
