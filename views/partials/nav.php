5cd4777d423db47f64d2daffa135626e73b05e59
Aranarora <aranarora007@gmail.com>
2023-10-17 14:25:56 -0700

    Create div in navbar with id Links and add pages links

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,7 +1,12 @@
 <header>
 
         <nav class="navbar">
-            
+            <div id="Links">
+            <a href="/">Home</a>
+                <a href="ride">Ride</a>
+                <a href="about">About</a>
+                <a href="contact">Contact Us</a>
+            </div>
         </nav>
 
 </header>
