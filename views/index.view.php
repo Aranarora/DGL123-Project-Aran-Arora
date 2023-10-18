aadaa234cc4c9480e0563f40fb52974f423641e5
Aranarora <aranarora007@gmail.com>
2023-10-17 18:23:42 -0700

    Create the another div with class slider in back slider

--- views/index.view.php
+++ views/index.view.php
@@ -3,7 +3,9 @@
 <?php require('partials/nav.php') ?>
 <main>
     <div class="back-slider">
-        
+        <div id="slider">
+            
+        </div>
     </div>
 </main>
 <?php require('partials/foot.php') ?>
