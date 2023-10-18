3f0b0a2ed4e90182d97523fe55d4a9d132eb9ce0
Aranarora <aranarora007@gmail.com>
2023-10-17 23:27:44 -0700

    Add div content-wrap around all content of 404 page

--- views/404.php
+++ views/404.php
@@ -3,9 +3,11 @@
   <?php require('partials/nav.php') ?>
 
   <main>
+    <div class="content-wrap">
       <h1>Sorry. Page Not Found.</h1>
       <p>
         <a class="btn" href = "/" >Go back home.</a>
       </p>
+</div>
   </main>
   <?php require('partials/foot.php') ?>
