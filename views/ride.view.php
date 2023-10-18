5eb04c42f5ee5f8dbee498dc6b1a45a45539d0b4
Aranarora <aranarora007@gmail.com>
2023-10-17 22:10:46 -0700

    Make allcards flex in row with space in between
    On large screen

--- views/ride.view.php
+++ views/ride.view.php
@@ -10,7 +10,7 @@
                 Experience the unique charm at the center of Comox Valley, all without the hassle of parking. Explore downtown Courtenay's hidden treasures by conveniently picking up an electric scooter at one of our two accessible locations.        </p>
             <p>
             In the global micromobility initiative, focused on reducing car dependency and urban traffic congestion, electric scooters emerge as the most efficient mode of transportation. The Long-Term Renewable City Plan in British Columbia sets a goal to transition all transportation and building energy consumption in the city to renewable sources by 2050. Achieving this target would not only enhance environmental and air quality but also contribute to the economic growth of British Columbia.</p>
-    </div>
+    
 
 
     <div class="cardssection">
