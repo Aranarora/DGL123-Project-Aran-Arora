4bd4bc966276e32765814b4831948ce9dfc3f144
Aranarora <aranarora007@gmail.com>
2023-10-17 22:28:58 -0700

    Create new div with class part1-div2 add content in it
    The imagination content

--- views/about.view.php
+++ views/about.view.php
@@ -7,6 +7,16 @@
 
 <div class="content-part1">
     <div class="part1-div1"><img src="imagefolder/illus-men.webp" alt="image of no face boy in illustration" style="width: 80%" /></div>
+    <div class="part1-div2">
+        <h2>RunRentals Journey</h2>
+        <div>
+            <p>
+            We aim to transform the transportation and exploration experience for individuals in Comox Valley. Go Scoot is excited to offer a reliable, eco-friendly electric scooter, ensuring your travels are both convenient and delightful.            </p>
+            <p>
+            Our fascination with e-scooters took root during the pandemic shutdown, a time of boredom and restlessness. The moment we hopped onto these robust two-wheeled wonders, we were instantly captivated! The city of Courtenay unfolds as a breathtaking backdrop, offering a picturesque and easily navigable terrain for swift exploration.            </p>
+            <p>
+            Out of this delightful meeting, the inception of RunRentals took place. In the spring of 2022, our storefront debuted in Comox Valley, situated at the core of downtown Courtenay.            </p>
+        </div>
     </div>
 </div>
 </div>
