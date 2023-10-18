32ee07658248ca28dd8e8f1c4586cf22caf0d24a
Aranarora <aranarora007@gmail.com>
2023-10-17 22:51:05 -0700

    Create the form wrapper div in the main part

--- views/contact.view.php
+++ views/contact.view.php
@@ -4,7 +4,9 @@
 <main class="content-wrap">
         <h1>Contact Us</h1>
 
-        
+        <div class="form-wrapper">
+            
+        </div>
 
     </main>
 <?php require('partials/foot.php') ?>
