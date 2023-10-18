f8d8650a687640c83b0f0acbc5a59467e9856357
Aranarora <aranarora007@gmail.com>
2023-10-17 23:20:44 -0700

    Add the 404 page basic structure that page not found

---
+++ views/404.php
@@ -0,0 +1,11 @@
+<?php require('partials/head.php') ?>
+
+  <?php require('partials/nav.php') ?>
+
+  <main>
+      <h1>Sorry. Page Not Found.</h1>
+      <p class = "mt-4">
+        <a href = "/" >Go back home.</a>
+      </p>
+  </main>
+  <?php require('partials/foot.php') ?>
