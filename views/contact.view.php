6af2f40409f9bb80fc44afb62a7c85791f32be18
Aranarora <aranarora007@gmail.com>
2023-10-17 22:50:28 -0700

    Add the main with class content-wrap and heading

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,5 +1,10 @@
 <?php require('partials/head.php') ?>
 
 <?php require('partials/nav.php') ?>
+<main class="content-wrap">
+        <h1>Contact Us</h1>
 
+        
+
+    </main>
 <?php require('partials/foot.php') ?>
