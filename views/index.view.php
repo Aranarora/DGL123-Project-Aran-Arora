c5fd8be3084b09a8b9995103936a48683f1dd83e
Aranarora <aranarora007@gmail.com>
2023-10-17 21:03:06 -0700

    Add the content related to the ride page to link it

--- views/index.view.php
+++ views/index.view.php
@@ -38,7 +38,12 @@
         </div>
       </div>
       <div class="content-wrap content-part2">
-    
+      <h2>Get Your Wheels</h2>
+        <h3>We create the difference?</h3>
+        <p>
+        Experience the unique charm at the center of Comox Valley, all without the hassle of parking. Explore downtown Courtenay's hidden treasures by conveniently picking up an electric scooter at one of our two accessible locations.        </p>
+        <p>
+        In the global micromobility initiative, focused on reducing car dependency and urban traffic congestion, electric scooters emerge as the most efficient mode of transportation. The Long-Term Renewable City Plan in British Columbia sets a goal to transition all transportation and building energy consumption in the city to renewable sources by 2050. Achieving this target would not only enhance environmental and air quality but also contribute to the economic growth of British Columbia.        </p>
       </div>
 </main>
 <?php require('partials/foot.php') ?>
