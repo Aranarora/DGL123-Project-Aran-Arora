b794df569a0100f204d63daf7e663043b9a75662
Aranarora <aranarora007@gmail.com>
2023-10-17 22:55:54 -0700

    Add the input type and place holder of the forms

--- views/contact.view.php
+++ views/contact.view.php
@@ -10,17 +10,18 @@
                 <label for="field-1">
              Full Name<span class="form-label-required"
               >* (required)</span></label>
-
+                <input type="text" id="field-1" name="placeholder-1" placeholder="Ex: Aran Arora" required/>
 
                 <label for="field-2">Mobile numbeer<span class="form-label-required"
               >* (required)</span></label>
-
+                <input type="text" id="field-2" name="placeholder-2" placeholder="Ex: 7781234567" required/>
 
                 <label for="field-3">E-mail<span class="form-label-optional"
               >* (optional)</span
             ></label>
-
+                <input type="text" id="field-3" name="placeholder-2" placeholder="Ex: aran1234@gmail.com" />
                 <div class="form-wrapper-button">
+                    <input type="submit" value="Submit" />
                 </div>
             </form>
         </div>
