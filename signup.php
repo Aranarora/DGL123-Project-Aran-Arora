bb13ce7ad179db11a7e5d87dcff5e20a1ff17e56
Aranarora <aranarora007@gmail.com>
2023-11-01 22:31:46 -0700

    Pass hashedpwd to make the data store in database
    And add $stmt null in end to stop the statement

--- signup.php
+++ signup.php
@@ -6,19 +6,13 @@ class Signup extends Database{
         $stmt = $this->connect()->prepare('INSERT INTO users (users_uid, users_pwd, users_email) VALUES (?, ?, ?);');
         $hashedpwd = password_hash($pwd, PASSWORD_DEFAULT);
 
-        if (!$stmt->excute(array($uid, $email))) {
+        if (!$stmt->excute(array($uid, $hashedpwd, $email))) {
             $stmt = null;
             abort(403);
             $error = 'The user already exist';
             exit();
         }
-        $resultCheck = 0;
-        if ($stmt->rowcount() > 0) {
-            $resultCheck = false;
-        } else {
-            $resultCheck = true;
-        }
-        return $resultCheck;
+        $stmt = null;
     }
 
     protected function checkUser($uid, $email){
