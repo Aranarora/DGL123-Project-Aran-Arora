2600020c947e78d55845ee806ea75a2e1983f5dc
Aranarora <aranarora007@gmail.com>
2023-11-01 21:43:15 -0700

    Make the execution of the function stop if it fails
    The data already exist so stop the function

--- signup.php
+++ signup.php
@@ -4,7 +4,8 @@ class Signup extends Database{
     protected function checkUser($uid, $email){
         $stmt = $this->connect()->prepare('SELECT users_uid from LoginTable WHERE users_uid = ? OR users_email = ?;');
 
-        if($stmt->excute(array($uid, $email))){
+        if(!$stmt->excute(array($uid, $email))){
+            $stmt = null;
         }
     }
 }
