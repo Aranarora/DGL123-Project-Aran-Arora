c85298f07baf7ebb287e4e26bbe1198e60b99681
Aranarora <aranarora007@gmail.com>
2023-11-01 21:31:58 -0700

    Make the connect function protected in database.php

--- Database.php
+++ Database.php
@@ -1,6 +1,13 @@
 <?php 
 class Database{
-    private function connect(){
-        
+    protected function connect(){
+        try{
+            $username = "root";
+            $password = "";
+            $dsn = new PDO('mysqli:host=localhost;dbname=LoginTable', $username, $password);
+        }
+        catch(PDOException $e){
+            throw new PDOException($e->getMessage(), $e->getCode());
+        }
     }
 }
