ba5af4c8e0e94774094ee676ab277a54f9326f2c
Aranarora <aranarora007@gmail.com>
2023-11-01 21:38:23 -0700

    Add a variable with query statement to check exist data

--- signup.php
+++ signup.php
@@ -2,6 +2,6 @@
 class Signup extends Database{
 
     protected function checkUser($uid, $email){
-        
+        $stmt = $this->connect()->prepare('SELECT users_uid from LoginTable WHERE users_uid = ? OR users_email = ?;');
     }
 }
