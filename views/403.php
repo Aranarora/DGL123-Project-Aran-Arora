21aeb5ce9f04446d90f112bf01eb631368873ce4
Aranarora <aranarora007@gmail.com>
2023-11-01 22:00:38 -0700

    Create 403 page with error variable to change value

---
+++ views/403.php
@@ -0,0 +1,13 @@
+<?php require('partials/head.php') ?>
+
+  <?php require('partials/nav.php') ?>
+
+  <main>
+    <div class="content-wrap">
+      <h1>Sorry. <?= $error ?></h1>
+      <p>
+        <a class="btn" href = "/" >Go back home.</a>
+      </p>
+</div>
+  </main>
+  <?php require('partials/foot.php') ?>
