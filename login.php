49eefb911bb280c04e578e63786909efc2058440
Aranarora <aranarora007@gmail.com>
2023-11-04 12:50:02 -0700

    Add stmt variable and pass the query it to connect

--- login.php
+++ login.php
@@ -4,7 +4,9 @@ class Login extends Database
 
     protected function getUser($uid, $psw)
     {
-        
+        $stmt = $this->connect()->prepare('SELECT users_psw FROM users WHERE users_uid = ? OR users_email = ?;');
+
+       
     }
 }
     
