c1d51a4d87132e60112d9265fd8e3c1c868f4981
Aranarora <aranarora007@gmail.com>
2023-11-04 12:45:30 -0700

    Add function to check user not passing empty input
    Taken from the controller signup page and passes to the get user function

--- controllers/login.php
+++ controllers/login.php
@@ -10,5 +10,23 @@ class loginContr extends Login
         $this->psw = $psw;
     }
 
-    
+    public function loginUser()
+    {
+        if ($this->emptyInput() == false) {
+            header("location: ../index.php?error=emptyinput");
+            exit();
+        }
+        
+        $this->getUser($this->uid, $this->psw);
+    }
+    private function emptyInput()
+    {
+        $result = false;
+        if (empty($this->uid) || empty($this->psw)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
 }
