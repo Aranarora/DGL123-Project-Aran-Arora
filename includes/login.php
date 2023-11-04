a738a9c3cd2ba83f8b2c90e51230b279100066e7
Aranarora <aranarora007@gmail.com>
2023-11-04 12:33:29 -0700

    Remove the pswrepeat and email variable from page

--- includes/login.php
+++ includes/login.php
@@ -2,15 +2,13 @@
 if (isset($_POST["submit"])) {
     $uid = $_POST["uid"];
     $psw = $_POST["psw"];
-    $pswrepeat = $_POST["pswrepeat"];
-    $email = $_POST["email"];
 
     include "../Database.php";
     include "../login.php";
     include "../controllers/login.php";
-    $login = new loginContr($uid, $psw, $pswrepeat, $email);
+    $login = new loginContr($uid, $psw);
 
     $login->loginUser();
 
-    header("location: ../?error=none");
+    header("location: ../?error=login");
 }
