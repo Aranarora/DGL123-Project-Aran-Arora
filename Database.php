c0ed755df939ecbb557cfb5a02a48aa4c4b93a0d
Aranarora <aranarora007@gmail.com>
2023-11-28 19:33:02 -0800

    Add the password root in the database site

--- Database.php
+++ Database.php
@@ -3,7 +3,7 @@ class Database{
     protected function connect(){
         try{
             $username = "root";
-            $password = "";
+            $password = "root";
             $dsn = new PDO('mysql:host=localhost;port=3306;dbname=users;charset=utf8mb4', $username, $password);
             return $dsn;
         }
